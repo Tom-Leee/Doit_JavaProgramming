349fd98f5f83a87a692b452cf6ef71705f21c97a
Server <Server@DESKTOP-QLDADKE>
2020-10-06 18:14:32 +0900

    2020-10-06

---
+++ DoIt_First/src/chapter2/Variable1.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int rating; //정수형 변수 rating을 선언
+		rating = 5; //rating 변수에 값 5를 입력
+		System.out.println(rating); //rating 값 출력
+		
+		String movieTitle; //문자열 변수 movieTitle을 선언
+		movieTitle = "기생충"; /* movieTitle 변수에 값 기생충을 입력
+		문자열의 값은 ""안에 내용을 적어주어야 한다. */
+		System.out.println(movieTitle); //movieTitle 값 출력
+	}
+
+}
